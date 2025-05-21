55c570a0cc092380209d05b233cb48096a7705fc
jenish savaliya <jenishsavaliya4022@gmail.com>
2025-05-21 13:40:19 +0530

    Header added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/header";
+import BannerContent from "../../components/bannerContent";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <BannerContent />
     </div>
   );
 };
